2e4e3d89428e390cb6491939b8817e3f53f4463e
santizoh <briansanti80@gamil.com>
2022-08-29 15:49:53 -0600

    CAMBIOS

--- MyBlog/resources/views/welcome.blade.php
+++ MyBlog/resources/views/welcome.blade.php
@@ -44,6 +44,7 @@
                         </g>
                     </svg>
                 </div>
+                <h1>HACIENDO PRUEBAS JAVIER MANCO </h1>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
